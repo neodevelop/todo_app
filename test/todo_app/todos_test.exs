361298bb4e054bd50ae8f65a1ee9a0e675a9bc4d
Jose Juan Reyes <neodevelop@gmail.com>
2021-09-27 17:50:54 -0500

    Agrego los archivos obtenidos del generador

---
+++ test/todo_app/todos_test.exs
@@ -0,0 +1,68 @@
+defmodule TodoApp.TodosTest do
+  use TodoApp.DataCase
+
+  alias TodoApp.Todos
+
+  describe "todos" do
+    alias TodoApp.Todos.Todo
+
+    @valid_attrs %{description: "some description", done: true, expiration: ~N[2010-04-17 14:00:00]}
+    @update_attrs %{description: "some updated description", done: false, expiration: ~N[2011-05-18 15:01:01]}
+    @invalid_attrs %{description: nil, done: nil, expiration: nil}
+
+    def todo_fixture(attrs \\ %{}) do
+      {:ok, todo} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Todos.create_todo()
+
+      todo
+    end
+
+    test "list_todos/0 returns all todos" do
+      todo = todo_fixture()
+      assert Todos.list_todos() == [todo]
+    end
+
+    test "get_todo!/1 returns the todo with given id" do
+      todo = todo_fixture()
+      assert Todos.get_todo!(todo.id) == todo
+    end
+
+    test "create_todo/1 with valid data creates a todo" do
+      assert {:ok, %Todo{} = todo} = Todos.create_todo(@valid_attrs)
+      assert todo.description == "some description"
+      assert todo.done == true
+      assert todo.expiration == ~N[2010-04-17 14:00:00]
+    end
+
+    test "create_todo/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Todos.create_todo(@invalid_attrs)
+    end
+
+    test "update_todo/2 with valid data updates the todo" do
+      todo = todo_fixture()
+      assert {:ok, %Todo{} = todo} = Todos.update_todo(todo, @update_attrs)
+      assert todo.description == "some updated description"
+      assert todo.done == false
+      assert todo.expiration == ~N[2011-05-18 15:01:01]
+    end
+
+    test "update_todo/2 with invalid data returns error changeset" do
+      todo = todo_fixture()
+      assert {:error, %Ecto.Changeset{}} = Todos.update_todo(todo, @invalid_attrs)
+      assert todo == Todos.get_todo!(todo.id)
+    end
+
+    test "delete_todo/1 deletes the todo" do
+      todo = todo_fixture()
+      assert {:ok, %Todo{}} = Todos.delete_todo(todo)
+      assert_raise Ecto.NoResultsError, fn -> Todos.get_todo!(todo.id) end
+    end
+
+    test "change_todo/1 returns a todo changeset" do
+      todo = todo_fixture()
+      assert %Ecto.Changeset{} = Todos.change_todo(todo)
+    end
+  end
+end
