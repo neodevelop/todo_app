82b5c54b14eee3f212a675f40cace34b4b37fa12
Jose Juan Reyes <neodevelop@gmail.com>
2021-09-30 17:03:48 -0500

    Agrego el cast de user_id y su validación

--- lib/todo_app/todos/todo.ex
+++ lib/todo_app/todos/todo.ex
@@ -1,8 +1,10 @@
 defmodule TodoApp.Todos.Todo do
   use Ecto.Schema
   import Ecto.Changeset
+  alias TodoApp.Todos.User
 
   schema "todos" do
+    belongs_to :user, User
     field :description, :string
     field :done, :boolean, default: false
     field :expiration, :naive_datetime
@@ -13,7 +15,18 @@ defmodule TodoApp.Todos.Todo do
   @doc false
   def changeset(todo, attrs) do
     todo
-    |> cast(attrs, [:description, :done, :expiration])
+    |> cast(attrs, [:description, :done, :expiration, :user_id])
     |> validate_required([:description, :done, :expiration])
+    |> foreign_key_constraint(:user_id)
+  end
+
+  @doc false
+  def changeset(_todo, :insert, _attrs) do
+    # ...
+  end
+
+  @doc false
+  def changeset(_todo, :update, _attrs) do
+    # ...
   end
 end
