5d6c188926f7ef13fcd71af33cda173de897c827
Jose Juan Reyes <neodevelop@gmail.com>
2021-09-30 19:01:06 -0500

    Agregamos el campo en la base de datos para todo_list dentro del todo

---
+++ priv/repo/migrations/20210930234523_relation_todo_list_todo.exs
@@ -0,0 +1,15 @@
+defmodule TodoApp.Repo.Migrations.RelationTodoListTodo do
+  use Ecto.Migration
+
+  def up do
+    alter table(:todos) do
+      add(:todo_list_id, references(:todos_lists, on_delete: :delete_all), null: false)
+    end
+  end
+
+  def down do
+    alter table(:todos) do
+      remove(:todo_list_id)
+    end
+  end
+end
