02ea437a81e269222ee4ee177406f89686633f9b
Jose Juan Reyes <neodevelop@gmail.com>
2021-10-05 17:51:58 -0500

    Habilito el uso de sokcet en JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,6 @@ import "../css/app.css"
 // Import deps with the dep name or local files with a relative path, for example:
 //
 //     import {Socket} from "phoenix"
-//     import socket from "./socket"
+import socket from "./socket"
 //
 import "phoenix_html"
