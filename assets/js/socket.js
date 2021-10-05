d619af83fa36a7c3207731d88cd645507da32c20
Jose Juan Reyes <neodevelop@gmail.com>
2021-10-05 17:52:19 -0500

    Agrego un observer para new_msg en JS

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -60,4 +60,6 @@ channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
 
+channel.on("new_msg", (payload) => { console.log(payload) })
+
 export default socket
