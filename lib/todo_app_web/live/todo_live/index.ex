4012ffd7c3bb539d7b69b047042bf5355929e380
Jose Juan Reyes <neodevelop@gmail.com>
2021-10-05 18:59:26 -0500

    Agrego elementos a render y el manejo del evento click

--- lib/todo_app_web/live/todo_live/index.ex
+++ lib/todo_app_web/live/todo_live/index.ex
@@ -4,6 +4,23 @@ defmodule TodoAppWeb.TodoLive.Index do
   def render(assigns) do
     ~L"""
     <h1>hola mundo</h1>
+    <h2>Como estas?</h2>
+    <h3><%= @message %></h3>
+    <button phx-click="increment">Incrementar</button>
+    <h3><%= @counter %></h3>
     """
+  end
+
+  def mount(_params, _session, socket) do
+    socket =
+      socket
+      |> assign(message: "Hello world", counter: 0)
+
+    {:ok, socket}
+  end
+
+  def handle_event("increment", params, socket) do
+    IO.inspect(params)
+    {:noreply, socket}
   end
 end
