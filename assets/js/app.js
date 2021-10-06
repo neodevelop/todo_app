9f0e23ece65e45df3197b04f2e1bfcf82b3153a2
Jose Juan Reyes <neodevelop@gmail.com>
2021-10-05 19:27:39 -0500

    Aplico la guía de instalación de LiveView

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,3 +13,14 @@ import "../css/app.css"
 import socket from "./socket"
 //
 import "phoenix_html"
+
+import {Socket} from "phoenix"
+import LiveSocket from "phoenix_live_view"
+
+let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
+let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+
+// Connect if there are any LiveViews on the page
+liveSocket.connect()
+
+window.liveSocket = liveSocket
--- mix.exs
+++ mix.exs
@@ -46,7 +46,9 @@ defmodule TodoApp.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:phx_gen_auth, "~> 0.7", only: [:dev], runtime: false}
+      {:phx_gen_auth, "~> 0.7", only: [:dev], runtime: false},
+      {:phoenix_live_view, "~> 0.15.0"},
+      {:floki, ">= 0.30.0", only: :test}
     ]
   end
 
